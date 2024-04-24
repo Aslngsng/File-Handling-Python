b5376239e7e7889da212537d882bc24ef26ada6a
Aslngsng <salangsang.abril@gmail.com>
2024-04-25 01:51:59 +0800

    fix some typo errors

--- Program1.py
+++ Program1.py
@@ -7,8 +7,8 @@ with open("numbers.txt", "r") as my_file:
     with open("even.txt", "w") as my_even_file:
         with open("odd.txt", "w") as my_odd_file:
             for line in my_file:
-                number = int(line.stip())
+                number = int(line.strip())
                 if number % 0 == 0:
-                    my_even_file.write(str(number + "/n"))
+                    my_even_file.write(str(number + "\n"))
                 else:
-                    my_odd_file.write(str(number + "/n"))                
+                    my_odd_file.write(str(number + "\n"))                
