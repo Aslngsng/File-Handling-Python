3b405dccb7f14956569470270af1cfeccd6176cd
Aslngsng <salangsang.abril@gmail.com>
2024-04-25 02:55:25 +0800

    fix some errors in the code

--- Program1.py
+++ Program1.py
@@ -3,12 +3,15 @@
 #iterate trough each line in numbers.txt
 #check if the numbers is even or odd and write the numbers to their designated txt
 
-with open("numbers.txt", "r") as my_file:
-    with open("even.txt", "w") as my_even_file:
-        with open("odd.txt", "w") as my_odd_file:
-            for line in my_file:
-                number = int(line.strip())
-                if number % 0 == 0:
-                    my_even_file.write(str(number + "\n"))
-                else:
-                    my_odd_file.write(str(number + "\n"))                
+def read_write():
+    with open("numbers.txt", "r") as my_file:
+      for line in my_file:
+        if int(line) % 2 == 0:
+            with open("even.txt", "a") as my_even_file:
+                my_even_file.write(str(line) + "\n")
+        else:
+            with open("odd.txt", "a") as my_odd_file:
+                    my_odd_file.write(str(line) + "\n")
+
+read_write()
+        
