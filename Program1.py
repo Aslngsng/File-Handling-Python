249b3738704738626263f2323e1ae9ca8519aef6
Aslngsng <salangsang.abril@gmail.com>
2024-04-25 01:01:49 +0800

    added code tha reads the numbers.txt

--- Program1.py
+++ Program1.py
@@ -1,4 +1,10 @@
 #create a program that reads the numbers.txt
-with open("numbers.txt", "r") as numbers_file:
-    for line in numbers_file:
-        print(line.strip)
+#create  a txt for odd and even
+#iterate trough each line in numbers.txt
+#check if the numbers is even or odd and write the numbers to their designated txt
+
+with open("numbers.txt", "r") as my_file:
+    for line in my_file:
+        line = line.strip
+        print(line)
+                       
