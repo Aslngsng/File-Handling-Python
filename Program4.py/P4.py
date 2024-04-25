4bd7630b3558676ea4246eb253f4602baa4702f4
Aslngsng <salangsang.abril@gmail.com>
2024-04-25 16:45:32 +0800

    open the integers.txt using read method

--- Program4.py/P4.py
+++ Program4.py/P4.py
@@ -2,3 +2,7 @@
 #read integers from source file
 #seperate the integers into even or odd numbers
 #write the integers to their respective file
+
+def process_integers(integer.txt):
+    with open("integers.txt", "r") as my_source:
+        numbers = my_source.readlines()
