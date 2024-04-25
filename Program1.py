b6667f70969f17661cc91005e352ea5aacdeffcc
Aslngsng <salangsang.abril@gmail.com>
2024-04-25 09:54:00 +0800

    fix some errors, and added notification message

--- Program1.py
+++ Program1.py
@@ -4,14 +4,15 @@
 #check if the numbers is even or odd and write the numbers to their designated txt
 
 def read_write():
-    with open("numbers.txt", "r") as my_file:
-      for line in my_file:
-        if int(line) % 2 == 0:
-            with open("even.txt", "a") as my_even_file:
-                my_even_file.write(str(line) + "\n")
+    with open("numbers.txt", "r") as my_file: 
+       with open("even.txt", "a") as my_even_file:
+        for line in my_file:
+          if int(line) % 2 == 0:
+           my_even_file.write(str(line) + "\n")
         else:
             with open("odd.txt", "a") as my_odd_file:
-                    my_odd_file.write(str(line) + "\n")
+              my_odd_file.write(str(line) + "\n")
 
 read_write()
+print("The even and odd numbers have been extracted and written to even.txt and odd.txt respectively")
         
