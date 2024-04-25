7a1a9fcb8e3910226963665cee70eff18fd90c98
Aslngsng <salangsang.abril@gmail.com>
2024-04-25 10:27:41 +0800

    added code that open and read the clas_data.txt

--- Program2.py/P2.py
+++ Program2.py/P2.py
@@ -1,3 +1,8 @@
 #read the data from the class.txt
 #compare the GWAs of the students to find the highest GWA
 #print the student who has highest GWA, including their GWA
+
+
+with open("class_data.txt", "r") as my_class_data:
+    line = my_class_data.realdines()
+
