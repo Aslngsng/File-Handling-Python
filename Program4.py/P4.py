d115d7ddf3260b838f48171b78edcc67b5c76e1a
Aslngsng <salangsang.abril@gmail.com>
2024-04-25 17:01:13 +0800

    added if else statement to partily the integers to even or odd

--- Program4.py/P4.py
+++ Program4.py/P4.py
@@ -6,3 +6,13 @@
 def process_integers(integer.txt):
     with open("integers.txt", "r") as my_source:
         numbers = my_source.readlines()
+
+    squared_even = []
+    cube_odd = []
+    for num in numbers:
+        num = int(num.strip())
+        if num % 2 == 0:
+            squared_even.append(num ** 2)
+        else:
+            cube_odd.append(num ** 3)
+            
