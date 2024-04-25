d846fe6f4904b42da22d00728d61406ca5cc1716
Aslngsng <salangsang.abril@gmail.com>
2024-04-25 14:50:50 +0800

    added conditonal statament to identify wether theres more than one who has highest GWA

--- Program2.py/P2.py
+++ Program2.py/P2.py
@@ -5,14 +5,18 @@
 
 with open("class_data.txt", "r") as my_class_data:
     lines = my_class_data.readlines()
-
 highest_GWA = 0.0
-highest_student_GWA = ""
+highest_student_GWAs = ""
 for line in lines:
     student_data = line.strip().split(",")
     student_name = student_data[0]
     GWA = float(student_data[1])
     if GWA >= highest_GWA:
         highest_GWA = GWA
-        highest_student_GWA = student_name
-print("The student(s) with highest GWA: " + highest_student_GWA + ", with GWA of " + str(highest_GWA))
+        highest_student_GWAs = student_name
+    elif GWA == highest_GWA:
+        highest_student_GWAs.append(student_name)
+if len (highest_student_GWAs) == 1:
+    print("The student with highest GWA: " + highest_student_GWAs + ", with GWA of " + str(highest_GWA))
+else:
+    print("The number of students with highest GWA: ", + ", " .join(highest_student_GWAs) + "with GWA of " + str(highest_GWA))
