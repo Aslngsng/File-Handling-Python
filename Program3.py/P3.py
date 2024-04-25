4a9a2614229160db00b88e104be3f2e19104f289
Aslngsng <salangsang.abril@gmail.com>
2024-04-25 15:57:44 +0800

    call the write to file function

--- Program3.py/P3.py
+++ Program3.py/P3.py
@@ -6,3 +6,9 @@
 def write_file(lines):
     with open ("mylife.txt", "w") as my_file:
         my_file.writelines("\n".join(lines))
+
+lines_to_write = ["You don't have to be a hero to save the world", 
+                "It doesn't make you a narcissist to love yourself", 
+                "It feels like nothing is easy, it'll never be",
+                "That's alright, let it out, talk to me" ]
+write_file(lines_to_write)
