233abac3a31de2122a2e23c02605a834eb29a75d
Aslngsng <salangsang.abril@gmail.com>
2024-04-25 17:54:56 +0800

    fix the error in the parameter

--- Program4.py/P4.py
+++ Program4.py/P4.py
@@ -3,8 +3,8 @@
 #seperate the integers into even or odd numbers
 #write the integers to their respective file
 
-def process_integers(integer.txt):
-    with open("integers.txt", "r") as my_source:
+def process_integers(source_filename):
+    with open(source_filename, "r") as my_source:
         numbers = my_source.readlines()
         
     squared_even = []
