ea4391fa1eab0c5530ad1834aa6b0c9165b9f608
Aslngsng <salangsang.abril@gmail.com>
2024-04-25 15:47:51 +0800

    added the fucntion

--- Program3.py/P3.py
+++ Program3.py/P3.py
@@ -2,3 +2,7 @@
 #open the file
 #write lines to the file 
 #apply the write to file
+
+def write_file(lines):
+    with open ("mylife.txt", "w") as my_file:
+        my_file.writelines("\n".join(lines))
