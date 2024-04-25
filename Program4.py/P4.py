f4571f5d18609267d57626a1b65bea6fc45439d4
Aslngsng <salangsang.abril@gmail.com>
2024-04-25 17:20:38 +0800

    write the integers to their respective file

--- Program4.py/P4.py
+++ Program4.py/P4.py
@@ -6,13 +6,23 @@
 def process_integers(integer.txt):
     with open("integers.txt", "r") as my_source:
         numbers = my_source.readlines()
-
+        
     squared_even = []
     cube_odd = []
-    for num in numbers:
+
+    for num in numbers: 
         num = int(num.strip())
         if num % 2 == 0:
             squared_even.append(num ** 2)
         else:
             cube_odd.append(num ** 3)
+    with open("double.txt" , "w") as my_even:
+        for square in squared_even:
+            my_even.write(str(square) +"\n")
+    with open("triple.txt" ,"w" ) as my_odd:
+        for cube in cube_odd:
+            my_odd.write(str(cube) + "\n")
             
+process_integers("integers.txt")
+
+
