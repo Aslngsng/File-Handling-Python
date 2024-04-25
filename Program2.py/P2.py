0af0e5edb13392a0c870adfde2bca83aac05515f
Aslngsng <salangsang.abril@gmail.com>
2024-04-25 18:29:26 +0800

    fix some errors

--- Program2.py/P2.py
+++ Program2.py/P2.py
@@ -6,17 +6,19 @@
 with open("class_data.txt", "r") as my_class_data:
     lines = my_class_data.readlines()
 highest_GWA = 0.0
-highest_student_GWAs = ""
+highest_student_GWAs = []
+
 for line in lines:
     student_data = line.strip().split(",")
     student_name = student_data[0]
     GWA = float(student_data[1])
-    if GWA >= highest_GWA:
+    if GWA > highest_GWA:
         highest_GWA = GWA
-        highest_student_GWAs = student_name
+        highest_student_GWAs = [student_name]
     elif GWA == highest_GWA:
         highest_student_GWAs.append(student_name)
+
 if len (highest_student_GWAs) == 1:
-    print("The student with highest GWA: " + highest_student_GWAs + ", with GWA of " + str(highest_GWA))
+    print("The student with highest GWA: " + highest_student_GWAs[0] + ", with GWA of " + str(highest_GWA))
 else:
-    print("The number of students with highest GWA: ", + ", " .join(highest_student_GWAs) + "with GWA of " + str(highest_GWA))
+    print("The number of students with highest GWA: " + ", ".join(highest_student_GWAs) + "with GWA of " + str(highest_GWA))
